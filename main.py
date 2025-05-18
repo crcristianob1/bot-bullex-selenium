14376e38d17f3713a021cac00bf5e7ec9eb9d7ba
crcristianob1 <crcristianob1@gmail.com>
2025-05-18 09:45:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-from bot import executar_bot
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot Bullex está rodando!"
+
+def iniciar_servidor():
+    app.run(host="0.0.0.0", port=10000)
 
 if __name__ == "__main__":
+    threading.Thread(target=iniciar_servidor).start()
+    # Aqui você chama sua função principal do bot
     executar_bot()
